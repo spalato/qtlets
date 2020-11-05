122e1a0dc1a86243024c2b4027e023f1c5be846e
SPalato <7659022+spalato@users.noreply.github.com>
2020-11-05 12:03:32 +0100

    Some more fun with Attr and Dataclasses

---
+++ scratch/try_dataclass.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+class Mixin(object):  # test mixin please ignore
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.param = []
+
+@dataclass
+class Data1(Mixin):
+    value: int = 1
+
+class Data2(Mixin):
+    value: int = 1
+
+d1 = Data1()
+d2 = Data2()
+
+assert d1.value == d2.value == 1
+assert not hasattr(d1, "param")
+assert     hasattr(d2, "param")
